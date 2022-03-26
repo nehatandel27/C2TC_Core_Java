2a1ce818f9584e5100e62d2ddf9fbbcb08d9426c
nehatandel27 <nehatandel9876@gmail.com>
2022-03-26 22:27:32 +0530

    IOC

---
+++ InversionOfControlDemo/src/main/java/com/tns/ioc/Airtel.java
@@ -0,0 +1,19 @@
+package com.tns.ioc;
+
+public class Airtel implements Sim{
+
+	public Airtel() {
+		System.out.println("i am airtel object");
+	}
+
+	public void calling() {
+		System.out.println("calling using airtel");
+		
+	}
+
+	public void data() {
+		
+		System.out.println("data using airtel");
+	}
+
+}
---
+++ InversionOfControlDemo/src/main/java/com/tns/ioc/Jio.java
@@ -0,0 +1,19 @@
+package com.tns.ioc;
+
+public class Jio implements Sim {
+
+	public Jio() {
+		System.out.println("i am jio object");
+	}
+	
+	public void calling() {
+		System.out.println("calling using Jio");
+		
+	}
+
+	public void data() {
+		System.out.println("data using jio");
+		
+	}
+
+}
